37f45e53255e9f38f835157a83c9f3ed8e1d5167
Lora Benjak <lora.benjak@infinum.com>
2024-05-22 22:51:47 +0200

    Add db migrations

---
+++ db/migrate/20240522204630_create_ratings.rb
@@ -0,0 +1,11 @@
+class CreateRatings < ActiveRecord::Migration[7.1]
+  def change
+    create_table :ratings do |t|
+      t.integer :score, null: false
+      t.string :review
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_204813) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.text "biography"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "book_ratings", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "rating_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_book_ratings_on_book_id"
+    t.index ["rating_id"], name: "index_book_ratings_on_rating_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "number_of_copies", default: 0, null: false
+    t.string "isbn", null: false
+    t.integer "pages", null: false
+    t.date "published_at", null: false
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "score", null: false
+    t.string "review"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_ratings_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email", null: false
+    t.string "role", default: "member", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "book_ratings", "books"
+  add_foreign_key "book_ratings", "ratings"
+  add_foreign_key "books", "authors"
+  add_foreign_key "ratings", "users"
 end
