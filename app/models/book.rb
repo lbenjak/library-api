20f0be46b491f66b6b79b43a6c2bbbcdd524180d
Lora Benjak <lora.benjak@infinum.com>
2024-05-26 17:33:44 +0200

    Fix destroy for books with ratings

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :author
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :book_genres, dependent: :destroy
   has_many :genres, through: :book_genres
 
   validates :title, presence: true
-  validates :number_of_copies, presence: true
-  validates :isbn, presence: true, uniqueness: true
+  validates :number_of_copies, presence: true, numericality: { only_integer: true }
+  validates :isbn, presence: true, uniqueness: { case_sensitive: false }
   validates :pages, presence: true, numericality: { only_integer: true }
   validates :published_at, presence: true
 
